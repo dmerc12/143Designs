3aa9326cd2d6e45c1053877dc8ed64e4b9cee2d2
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-12-04 16:43:37 -0600

    adds home page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,15 +3,20 @@ import './App.css';
 import { useRef } from 'react';
 import { ToastContainer } from './components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login, ManageInformation } from './pages';
+import { Home, Login, ManageInformation } from './pages';
+import { Navbar } from './components'
 
 function App() {
   const toast = useRef().current.addToast;
 
   return (
     <>
+      <Navbar toast={toast} />
+
       <BrowserRouter>
         <Routes>
+          <Route index path="/" element={<Home toast={toast}/>} />
+          <Route index path="/home" element={<Home toast={toast}/>} />
           <Route index path='/login' element={<Login toast={toast} />} />
           <Route index path='/manage/information' element={<ManageInformation toast={toast} />} />
         </Routes>
