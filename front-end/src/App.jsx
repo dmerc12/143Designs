b03a7bdf86500e40048b6f26fea6a32284cb5b60
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-12-08 11:39:28 -0600

    updates create user form, as well as app.jsx and manage users page for later development

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -17,13 +17,13 @@ function App() {
           {/* <Route index path="/" element={<Home toast={toast}/>} />
           <Route index path="/home" element={<Home toast={toast}/>} /> */}
           <Route path='/login' element={<Login toast={toast} />} />
-          {/* <Route index path='/manage/information' element={<ManageUsers toast={toast} />} />
-          <Route index path='/manage/requests' element={<ManageRequests toast={toast} />} />
-          <Route index path='/manage/reviews' element={<ManageReviews toast={toast} />} />
-          <Route index path='/manage/past/work' element={<ManagePastWork toast={toast} />} />
-          <Route index path='/contact' element={<Contact toast={toast} />} />
-          <Route index path='/reviews' element={<Reviews toast={toast} />} />
-          <Route index path='/past/work' element={<PastWork toast={toast} />} /> */}
+          <Route path='/manage/information' element={<ManageUsers toast={toast} />} />
+          {/* <Route index path='/manage/requests' element={<ManageRequests toast={toast} />} /> */}
+          {/* <Route index path='/manage/reviews' element={<ManageReviews toast={toast} />} /> */}
+          {/* <Route index path='/manage/past/work' element={<ManagePastWork toast={toast} />} /> */}
+          {/* <Route index path='/contact' element={<Contact toast={toast} />} /> */}
+          {/* <Route index path='/reviews' element={<Reviews toast={toast} />} /> */}
+          {/* <Route index path='/past/work' element={<PastWork toast={toast} />} /> */}
 
         </Routes>
       </BrowserRouter>
