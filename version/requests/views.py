92cf11edecb48206c1a9416ff4fea06dd41793e2
unknown <dylanmercer12@icloud.com>
2023-12-13 16:26:00 -0600

    adds login required to main home pages, need to create a separate home page for when a user logs in and let the normal page be the public page

--- version/requests/views.py
+++ version/requests/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return HttpResponse('<h1>Requests Home</h1>')
--- version/users/views.py
+++ version/users/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
