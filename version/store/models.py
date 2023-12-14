e106a28deb75332e6788bd68e0c775e0cd9e721c
unknown <dylanmercer12@icloud.com>
2023-12-13 19:09:42 -0600

    adds all functionality for create, read, update, and delete  for items but no buttons made yet in the pages to use

--- version/store/models.py
+++ version/store/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Item(models.Model):
     name = models.CharField(max_length=60)
 
     def __str__(self):
-        return f'Name: {self.name} \n'
+        return f'ID: {self.pk} - Name: {self.name} \n'
+
+    def get_absolute_url(self):
+        return reverse('store-manage-items')
 
 class Order(models.Model):
     name = models.CharField(max_length=60)
@@ -16,8 +21,11 @@ class Order(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return (f"Name: {self.name} - Complete: {self.complete} - Paid: {self.paid} - Description: {self.description} "
-                f"- Created: {self.created} - Last Modified: {self.last_modified} \n")
+        return (f"ID: {self.pk} - Name: {self.name} - Complete: {self.complete} - Paid: {self.paid} - "
+                f"Description: {self.description} - Created: {self.created} - Last Modified: {self.last_modified} \n")
+
+    def get_absolute_url(self):
+        return reverse('store-manage-orders')
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -25,4 +33,7 @@ class OrderItem(models.Model):
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"Order Name: {self.order.name} - Item: {self.item.name} - Quantity: {self.quantity} \n"
+        return f"ID: {self.pk} - Order Name: {self.order.name} - Item: {self.item.name} - Quantity: {self.quantity} \n"
+
+    def get_absolute_url(self):
+        return reverse('store-manage-order-items')
