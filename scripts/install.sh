39799b8c381984680ba2e21819353ad55a8ea92a
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-12-18 23:03:11 -0600

    adds start of install script

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+# update package lists
+sudo apt update
+# install python
+sudo apt install python3
+# install pip
+sudo apt install python3-pip
+# install git
+sudo apt install git
+# install project from github
+git clone https://github.com/dmerc12/143Designs.git
+# change into project
+cd 143Designs
+# create virtual environment
+# activate virtual environment
+# install django and other dependencies
+pip install django crispy-bootstrap5 crispy-forms
+# change into django project
+# 
