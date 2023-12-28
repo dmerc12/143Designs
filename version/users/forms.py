e60984f114f93b0b0586844c9fbb3d85a60c6ae9
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-12-27 21:49:04 -0600

    adds change password form, updates other user forms

--- version/users/forms.py
+++ version/users/forms.py
@@ -1,17 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-
+class AddUserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password', 'confirmation_password']
 
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-
+class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class ChangePasswordForm(PasswordChangeForm):
+    pass
