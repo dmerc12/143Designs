6487053832aced91f30e60fe6132d4e601d65cce
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-12-29 21:27:49 -0600

    adds checks to user middleware

--- version/users/middleware.py
+++ version/users/middleware.py
@@ -1,3 +1,4 @@
+import logging
 from django.contrib import messages
 
 class UserMiddleware:
@@ -5,10 +6,49 @@ class UserMiddleware:
     @staticmethod
     def create_user(request, form):
         username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password')
         confirmation_password = form.cleaned_data.get('confirmation_password')
-        form.save()
-        messages.success(request, f'Account created for {username}!')
+        logging.info('Beginning user middleware method create user with username: ' + str(username) + ', email: ' + str(email) + ', password: ' + str(password) + ', and confirmation password: ' + str(confirmation_password))
+        if type(username) is not str:
+            logging.warning('Error in user middleware method create user, username not a string')
+            raise RuntimeError('The username field must be a string, please try again!')
+        elif len(username) > 60:
+            logging.warning('Error in user middleware create user, username too long')
+            raise RuntimeError('The username field cannot exceed 60 characters, please try again!')
+        elif username == '':
+            logging.warning('Error in user middleware method create user, username left empty')
+            raise RuntimeError('The username field cannot be left empty, please try again!')
+        elif type(email) is not str:
+            logging.warning('Error in user middleware method create user, email not a string')
+            raise RuntimeError('The email field must be a string, please try again!')
+        elif len(email) > 60:
+            logging.warning('Error in user middleware method create user, email too long')
+            raise RuntimeError('The email field cannot exceed 60 characters, please try again!')
+        elif email == '':
+            logging.warnig('Error in user middleware method create user email left empty')
+            raise RuntimeError('The email field cannot be left empty, please try again!')
+        elif type(password) is not str:
+            logging.warning('Error in user middleware method create user, password not a string')
+            raise RuntimeError('The password field must be a string, please try again!')
+        elif len(password) > 60:
+            logging.warning('Error in user middleware method create user, password too long')
+            raise RuntimeError('The password field cannot exceed 60 characters, please try again!')
+        elif password == '':
+            logging.warning('Error in user middleware method create user, password left empty')
+            raise RuntimeError('The password field cannot be left empty, please try again!')
+        elif type(confirmation_password) is not str:
+            logging.warning('Error in user middleware method create user, confirmation password not a string')
+            raise RuntimeError('The confirmation password must be a string, please try again!')
+        elif len(confirmation_password) > 60:
+            logging.warning('Error in user middleware method create user, confirmation password too long')
+            raise RuntimeError('The confirmation password cannot exceed 60 characters, please try again!')
+        elif confirmation_password == '':
+            logging.warning('Error in user middleware method create user, confirmation password left empty')
+            raise RuntimeError('The confirmation password cannot be left empty, please try again!')
+        else:
+            form.save()
+            messages.success(request, f'Account created for {username}!')
 
     @staticmethod
     def update_user(request, form):
