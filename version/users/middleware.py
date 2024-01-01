dd508ca460b072f78130c617425de71345fd96af
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-12-31 19:04:27 -0600

    fixes update user and finishes change password for user middleware

--- version/users/middleware.py
+++ version/users/middleware.py
@@ -9,7 +9,7 @@ class UserMiddleware:
         email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password')
         confirmation_password = form.cleaned_data.get('confirmation_password')
-        logging.info('Beginning user middleware method create user with username: ' + str(username) + ', email: ' + str(email) + ', password: ' + str(password) + ', and confirmation password: ' + str(confirmation_password))
+        logging.info('Beginning user middleware method create user with username: ' + str(username) + ', email: ' + str(email))
         if type(username) is not str:
             logging.warning('Error in user middleware method create user, username not a string')
             raise RuntimeError('The username field must be a string, please try again!')
@@ -55,9 +55,7 @@ class UserMiddleware:
     def update_user(request, form):
         username = form.cleaned_data.get('username')
         email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        confirmation_password = form.cleaned_data.get('confirmation_password')
-        logging.info('Beginning user middleware method update user with username: ' + str(username) + ', email: ' + str(email) + ', password: ' + str(password) + ', and confirmation password: ' + str(confirmation_password))
+        logging.info('Beginning user middleware method update user with username: ' + str(username) + ', email: ' + str(email))
         if type(username) is not str:
             logging.warning('Error in user middleware method update user, username not a string')
             raise RuntimeError('The username field must be a string, please try again!')
@@ -76,24 +74,6 @@ class UserMiddleware:
         elif email == '':
             logging.warnig('Error in user middleware method update user email left empty')
             raise RuntimeError('The email field cannot be left empty, please try again!')
-        elif type(password) is not str:
-            logging.warning('Error in user middleware method update user, password not a string')
-            raise RuntimeError('The password field must be a string, please try again!')
-        elif len(password) > 60:
-            logging.warning('Error in user middleware method update user, password too long')
-            raise RuntimeError('The password field cannot exceed 60 characters, please try again!')
-        elif password == '':
-            logging.warning('Error in user middleware method update user, password left empty')
-            raise RuntimeError('The password field cannot be left empty, please try again!')
-        elif type(confirmation_password) is not str:
-            logging.warning('Error in user middleware method update user, confirmation password not a string')
-            raise RuntimeError('The confirmation password must be a string, please try again!')
-        elif len(confirmation_password) > 60:
-            logging.warning('Error in user middleware method update user, confirmation password too long')
-            raise RuntimeError('The confirmation password cannot exceed 60 characters, please try again!')
-        elif confirmation_password == '':
-            logging.warning('Error in user middleware method update user, confirmation password left empty')
-            raise RuntimeError('The confirmation password cannot be left empty, please try again!')
         else:
             current_info = request.user
             if current_info.username == username and current_info.email == email:
@@ -106,8 +86,31 @@ class UserMiddleware:
 
     @staticmethod
     def change_password(request, form):
-        form.save()
-        messages.success(request, 'Your password has been changed!')
+        password = form.cleaned_data.get('password')
+        confirmation_password = form.cleaned_data.get('confirmation_password')
+        logging.info('Beginning user middleware method change password')
+        if type(password) is not str:
+            logging.warning('Error in user middleware method change password, password not a string')
+            raise RuntimeError('The password field must be a string, please try again!')
+        elif len(password) > 60:
+            logging.warning('Error in user middleware method change password, password too long')
+            raise RuntimeError('The password field cannot exceed 60 characters, please try again!')
+        elif password == '':
+            logging.warning('Error in user middleware method change password, password empty')
+            raise RuntimeError('The password field cannot be left empty, please try again!')
+        elif type(confirmation_password) is not str:
+            logging.warning('Error in user middleware method change password, confirmation password not a string')
+            raise RuntimeError('The confirmation password field must be a string, please try again!')
+        elif len(confirmation_password) > 60:
+            logging.warning('Error in user middleware method change password, confirmation password too long')
+            raise RuntimeError('The confirmation password field cannot exceed 60 characters, please try again!')
+        elif confirmation_password == '':
+            logging.warning('Error in user middleware method change password, confirmation password empty')
+            raise RuntimeError('The confirmation password field cannot be left empty, please try again!')
+        else:
+            logging.info('Finishing user middleware method change password')
+            form.save()
+            messages.success(request, 'Your password has been changed!')
 
     @staticmethod
     def delete_user(request):
