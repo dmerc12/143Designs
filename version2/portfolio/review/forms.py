c03d76f6eec00c0173a33e67219faa6506e03a4f
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-01-03 23:55:49 -0600

    alters review model, review form and create review template

--- version2/portfolio/review/forms.py
+++ version2/portfolio/review/forms.py
@@ -4,6 +4,4 @@ from ..models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['name', 'text', 'rating']
-
-    rating = forms.ChoiceField(choices=[(i / 2, str(i / 2)) for i in range(21)], widget=forms.RadioSelect)
+        fields = ['first_name', 'last_name', 'subject', 'text', 'rating']
