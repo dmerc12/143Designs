a9320e17a87fd7153f6f4b3dc93ffb59c49a6e0e
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-01-21 17:41:15 -0600

    alters create order view so that initial order item form renders though still not submitting

--- version2/store/order/views.py
+++ version2/store/order/views.py
@@ -2,25 +2,32 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from ..forms import OrderForm, OrderItemForm
 from django.contrib import messages
+from django.forms import formset_factory
 from ..models import Order, OrderItem
 
 @login_required
 def create_order(request):
+    OrderItemFormSet = formset_factory(OrderItemForm, extra=1)
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            order_item_forms = request.POST.getlist('order_item_form')
-            for item_form_data in order_item_forms:
-                item_form = OrderItemForm(item_form_data)
+        order_form = OrderForm(request.POST)
+        order_item_forms = OrderItemFormSet(request.POST)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_form in order_item_forms:
                 if item_form.is_valid():
-                    order_item = OrderItem.objects.create(order=order, item=order_item.cleaned_data['item'], quantity=order_item.cleaned_data['quantity'])
+                    item = item_form.cleaned_data['item']
+                    quantity = item_form.cleaned_data['quantity']
+                    OrderItem.objects.create(order=order, item=item, quantity=quantity)
             messages.success(request, 'Order successfully created!')
             return redirect('store-home')
     else:
         order_form = OrderForm()
-        order_item_form = OrderItemForm()
-    return render(request, 'store/order/create.html', {'order_form': order_form, 'order_item_form': order_item_form})
+        order_item_forms = OrderItemFormSet()
+    context = {
+        'order_form': order_form, 
+        'order_item_forms': order_item_forms
+    }
+    return render(request, 'store/order/create.html', context)
 
 @login_required
 def order_detail(request, order_id):
