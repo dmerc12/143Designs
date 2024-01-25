36a29dd182170cefc1c984980b87dd1f877077a4
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-01-24 21:51:10 -0600

    adds what should by dynamic creating order item forms for the update order page

--- version2/store/order_item/views.py
+++ version2/store/order_item/views.py
@@ -2,20 +2,28 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from ..forms import OrderItemForm
-from..models import Order
+from..models import Order, OrderItem
 
 @login_required
-def create_order_item(request, order_id):
-    order = get_object_or_404(Order, pk=order_id)
-    form = OrderItemForm(request.POST)
-    if form.is_valid():
-        order_item = form.save(commit=False)
-        order_item.order = order
-        order_item.save()
-        messages.success(request, 'Item successfully added to order!')
-        return redirect('store-order-update', order.pk)
-        
+def get_item_form(request):
+    return render(request, 'store/partials/form.html', {'item_form': OrderItemForm()})
+
 @login_required
-def add_new_form(request):
-    form = OrderItemForm()
-    return render(request, 'store/partials/form.html', {'item_form': form})
+def create_order_item(request, order_id=0):
+    order = get_object_or_404(Order, pk=order_id)
+    if request.method == 'POST':
+        form = OrderItemForm(request.POST)
+        if form.is_valid():
+            order_item = form.save(commit=False)
+            order_item.order = order
+            order_item.save()
+            messages.success(request, 'Item successfully added to order!')
+            redirect('store-order-update', order.pk)
+        else:
+            return render(request, 'store/partials/form.html', {'item_form': form})
+    else:
+        return render(request, 'store/partials/form.html', {'item_form': OrderItemForm()})
+
+@login_required
+def update_order_item(request):
+    pass
