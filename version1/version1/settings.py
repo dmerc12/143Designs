836e4129933551692edbfc48d59b68ad76b0f1a3
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-01-24 23:53:25 -0600

    updates settings for jazzmin ui builder

--- version1/version1/settings.py
+++ version1/version1/settings.py
@@ -128,3 +128,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+
+}
+
+JAZZMIN_SETTINGS["show_ui_builder"] = True
