fbc6bbf0bd4705d5f733a592b27931154a06365e
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-02-04 23:55:59 -0600

    adds detail page for store items, working through rendering sizes and prices though

--- version1/store/urls.py
+++ version1/store/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', store_home, name='store-home'),
+    path('item/<int:item_id>/', item_detail, name='item-detail')
 ]
--- version1/store/views.py
+++ version1/store/views.py
@@ -6,3 +6,17 @@ def store_home(request):
     items = Item.objects.filter(featured=True)
     context = {'items': items}
     return render(request, 'store/index.html', context)
+
+def item_detail(request, item_id):
+    item = Item.objects.get(id=item_id)
+    related_items = Item.objects.filter(category=item.category)
+    sizes = {}
+    for size in item.product.get_product_sizes():
+        size_price = size.price + item.design.price
+        sizes[size.size] = size_price
+    context = {
+        'item': item,
+        'sizes': sizes,
+        'related_items': related_items
+    }
+    return render(request, 'store/detail.html', context)
