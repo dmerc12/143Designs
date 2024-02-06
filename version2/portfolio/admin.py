36606f6152461399a007badbd9d412943c00c5c4
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-02-05 23:19:11 -0600

    removes version 2 that was outdated

--- version2/portfolio/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Work, Review
-
-admin.site.register(Work)
-admin.site.register(Review)
--- version2/store/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Order, Item, OrderItem
-
-admin.site.register(Item)
-admin.site.register(Order)
-admin.site.register(OrderItem)
