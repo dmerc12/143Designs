8be99e20724a6d1212880ee391f056bcd3d0263d
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-02-06 22:16:35 -0600

    moves navbar into separate file to be included in base template, adds comments to rest of admin and model files in project

--- version1/order_tracking/admin.py
+++ version1/order_tracking/admin.py
@@ -3,31 +3,38 @@ from django.utils.html import format_html
 from django.contrib import admin
 from .models import Order
 
+# Unregisters default group and user models from admin site
 admin.site.unregister(Group)
 admin.site.unregister(User)
 
+# Inline for items in an order in the admin site
 class OrderItemInline(admin.TabularInline):
     model = Order.item.through
     extra = 1
     verbose_name = 'Item'
     verbose_name_plural = 'Items'
 
+# Adds an action to mark selected orders as complete
 @admin.action(description='Mark selected orders as complete')
 def mark_complete(modeladmin, request, queryset):
     queryset.update(complete=True)
 
+# Adds an action to mark selected orders as incomplete
 @admin.action(description='Mark selected orders as incomplete')
 def mark_incomplete(modeladmin, request, queryset):
     queryset.update(complete=False)
 
+# Adds an action to mark selected orders as paid
 @admin.action(description='Mark selected orders as paid')
 def mark_paid(modeladmin, request, queryset):
     queryset.update(paid=True)
 
+# Adds an action to mark selected orders as not paid
 @admin.action(description='Mark selected orders as not paid')
 def mark_unpaid(modeladmin, request, queryset):
     queryset.update(paid=False)
 
+# Registers orders with the admin site
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['custom_id_display', 'customer', 'short_description', 'total', 'complete', 'paid', 'last_modified', 'created']
@@ -45,7 +52,9 @@ class OrderAdmin(admin.ModelAdmin):
         }),
     )
 
+    # Displays a custom ID for the orders
     def custom_id_display(self, obj):
         return format_html(f'143DORD{obj.id}')
-    
+
+    # Alters column header for custom ID function above
     custom_id_display.short_description = 'Order Number'
--- version1/store/admin.py
+++ version1/store/admin.py
@@ -2,28 +2,35 @@ from .models import Category, Item, ItemImages
 from django.utils.html import format_html
 from django.contrib import admin
 
+# Registers categories with admin site
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['custom_id', 'name', 'image_preview']
     
+    # Displays a custom ID for the categories
     def custom_id(self, obj):
         return format_html(f'143DCAT{obj.id}')
-    
+
+    # Alters column header for custom ID function above 
     custom_id.short_description = 'Category ID'
 
+# Inline for item images in admin site
 class ItemImagesInline(admin.TabularInline):
     model = ItemImages
     extra = 1
     verbose_name = 'Images'
     verbose_name_plural = 'Images'
 
+# Registers items with admin site
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     inlines = [ItemImagesInline]
     list_display = ['custom_id', 'name', 'category', 'featured', 'sale', 'image_preview']
     list_filter = ['category', 'featured', 'sale']
-    
+
+    # Displays a custom ID for the items
     def custom_id(self, obj):
         return format_html(f'143DIT{obj.id}')
     
+    # Alters column header for custom ID function above
     custom_id.short_description = 'Item ID'
