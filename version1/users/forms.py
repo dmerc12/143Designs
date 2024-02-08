235d1b3645ad0a086e92841963c2699a48532696
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-02-08 00:18:40 -0600

    updates login view to use custom login django form rather than the static form that was present

--- version1/users/forms.py
+++ version1/users/forms.py
@@ -2,6 +2,24 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, SetPassw
 from django.contrib.auth.models import User
 from django import forms
 
+# Login form for customers
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Enter your username.</small></span>'
+
+        self.fields['password'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].label = ''
+        self.fields['password'].help_text = '<span class="form-text text-muted"><small>Enter your password.</small></span>'
+		
 # Register form for new customers
 class SignUpForm(UserCreationForm):
 	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
--- version1/users/views.py
+++ version1/users/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm, UpdateCustomerForm, ChangePasswordForm
+from .forms import LoginForm, SignUpForm, UpdateCustomerForm, ChangePasswordForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # Login view for customer side of website
 def login_customer(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        customer = authenticate(request, username=username, password=password)
-        if customer is not None:
-            login(request, customer)
-            messages.success(request, 'You have been successfully logged in!')
-            return redirect('store-home')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            customer = authenticate(request, username=username, password=password)
+            if customer is not None:
+                login(request, customer)
+                messages.success(request, 'You have been successfully logged in!')
+                return redirect('store-home')
+            else:
+                messages.error(request, 'Incorrect username or password, please try again!')
+                return redirect('login')
         else:
             messages.error(request, 'Incorrect username or password, please try again!')
             return redirect('login')
     else:
-        return render(request, 'users/login.html')
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
 
 # Logout view for customer side of website
 def logout_customer(request):
@@ -47,6 +53,7 @@ def register_customer(request):
         form = SignUpForm()
         return render(request, 'users/register.html', {'form': form})
 
+# Update customer profile view for customer side of website
 def update_customer(request):
     if request.user.is_authenticated:
         current_user = User.objects.get(id=request.user.id)
@@ -62,6 +69,7 @@ def update_customer(request):
         messages.error(request, 'You must be logged in to access this page. Please log in then try again!')
         return redirect('home')
 
+# Change password view for customer side of website
 def change_password(request):
     if request.user.is_authenticated:
         current_user = request.user
@@ -69,8 +77,9 @@ def change_password(request):
             form = ChangePasswordForm(current_user, request.POST)
             if form.is_valid():
                 form.save()
-                messages.success(request, 'Your password has been changed. Please login again!')
-                return redirect('login')
+                login(request, current_user)
+                messages.success(request, 'Your password has been changed!')
+                return redirect('update-customer')
             else:
                 for error in list(form.errors.values()):
                     messages.error(request, error)
