78d8e863fdca553e0da932632742eef210454c74
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-03-04 18:42:18 -0600

    adds tests for login and register forms

--- version1/users/tests.py
+++ version1/users/tests.py
@@ -3,6 +3,7 @@ from .admin import CustomerAdmin, SupplierAdmin
 from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import *
+from .forms import *
 
 # Tests for users models
 class TestUsersModels(TestCase):
@@ -70,4 +71,144 @@ class TestUsersForms(TestCase):
 
     # Setup before tests
     def setUp(self):
-        pass
+        self.user = User.objects.create_user(username='test_user', email='user@example.com', password='testpassword', first_name='user', last_name='example')
+        
+    # Tests for login form 
+    # Test form initialization
+    def test_login_form_initialization(self):
+        form = LoginForm()
+        self.assertIn('username', form.fields.keys())
+        self.assertIn('password', form.fields.keys())
+     
+    # Test form validation empty fields 
+    def test_login_form_validation_empty_fields(self):
+        data = {
+            'username': '',
+            'password': ''
+        }
+        form = LoginForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors)
+        self.assertIn('password', form.errors)
+
+    # Test form validation success
+    def test_login_form_validation_success(self):
+        data = {
+            'username': self.user.username,
+            'password': self.user.password
+        }
+        form = LoginForm(data=data)
+        self.assertTrue(form.is_valid())
+        
+    # Tests for register form 
+    # Test form initialization
+    def test_register_form_initialization(self):
+        form = SignUpForm()
+        self.assertIn('username', form.fields.keys())
+        self.assertIn('password1', form.fields.keys())
+        self.assertIn('password2', form.fields.keys())
+        self.assertIn('phone_number', form.fields.keys())
+        self.assertIn('email', form.fields.keys())
+        self.assertIn('first_name', form.fields.keys())
+        self.assertIn('last_name', form.fields.keys())
+
+     
+    # Test form validation empty fields 
+    def test_register_form_validation_empty_fields(self):
+        data = {
+            'username': '',
+            'password1': '',
+            'password2': '',
+            'phone_number': '',
+            'email': '',
+            'first_name': '',
+            'last_name': ''
+        }
+        form = SignUpForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors)
+        self.assertIn('password1', form.errors)
+        self.assertIn('password2', form.errors)
+        self.assertIn('phone_number', form.errors)
+        self.assertIn('email', form.errors)
+        self.assertIn('first_name', form.errors)
+        self.assertIn('last_name', form.errors)
+
+    # Test form validation fields too long
+    def test_register_form_validation_fields_too_long(self):
+        data = {
+            'username': 'this is just a test so it is designed to fail if it is a good test and so then it will cause the form not to be valid and the due to those failures the form will not be valid and an error message will ensue',
+            'password1': self.user.password,
+            'password2': self.user.password,
+            'phone_number': '1-425-345-6948-19371-283',
+            'email': 'testthatthisistoolongsoithastobesuperlongandthentheformwillnotbevalidandthenthetestwillpassandbesuccessfulbutinordertodosoithastobeincorrectformatsoitgetspastthefirstchecksuntilitreachesthelengthcheck@example.com',
+            'first_name': 'this is just a test so it is designed to fail if it is a good test and so then it will cause the form not to be valid and the due to those failures the form will not be valid and an error message will ensue',
+            'last_name': 'this is just a test so it is designed to fail if it is a good test and so then it will cause the form not to be valid and the due to those failures the form will not be valid and an error message will ensue'
+        }
+        form = SignUpForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors)
+        self.assertIn('phone_number', form.errors)
+        self.assertIn('email', form.errors)
+        self.assertIn('first_name', form.errors)
+        self.assertIn('last_name', form.errors)
+
+    # Test form validation email format incorrect
+    def test_register_form_validation_invalid_email_format(self):
+        data = {
+            'username': 'testuser',
+            'password1': 'mismatching',
+            'password2': 'passwords',
+            'phone_number': '1-425-345-6948',
+            'email': 'test at example dot com',
+            'first_name': 'test',
+            'last_name': 'user'
+        }
+        form = SignUpForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors)
+
+    # Test form validation password invalid
+    def test_register_form_validation_password_invalid(self):
+        data = {
+            'username': 'testuser',
+            'password1': 'testuser',
+            'password2': 'testuser',
+            'phone_number': '1-425-345-6948',
+            'email': 'test@example.com',
+            'first_name': 'test',
+            'last_name': 'user'
+        }
+        form = SignUpForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('password2', form.errors)
+
+    # Test form validation mismatching passwords
+    def test_register_form_validation_mismatching_passwords(self):
+        data = {
+            'username': 'testuser',
+            'password1': 'mismatching',
+            'password2': 'passwords',
+            'phone_number': '1-425-345-6948',
+            'email': 'test@example.com',
+            'first_name': 'test',
+            'last_name': 'user'
+        }
+        form = SignUpForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('password2', form.errors)
+
+    # Test form validation success
+    def test_register_form_validation_success(self):
+        data = {
+            'username': 'yet_another_one',
+            'password1': self.user.password,
+            'password2': self.user.password,
+            'phone_number': '1-425-345-6948',
+            'email': 'test@example.com',
+            'first_name': 'test',
+            'last_name': 'user'
+        }
+        form = SignUpForm(data=data)
+        self.assertTrue(form.is_valid())
+        
