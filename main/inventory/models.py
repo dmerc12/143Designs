bccc3b438dbbc7ac7cd49a1400548eb526f21182
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-03-19 22:12:32 -0500

    adds models and tests for supplier, product, purchase, design, product size, purchase product, and purchase design; all models needed for inventory app

--- main/inventory/models.py
+++ main/inventory/models.py
@@ -1,3 +1,90 @@
+from django.utils.html import format_html
+from users.models import Supplier
 from django.db import models
+from itertools import chain
 
-# Create your models here.
+# Model for physical product
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    material = models.CharField(max_length=150)
+    color = models.CharField(max_length=150)
+    description = models.TextField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.color} {self.material} {self.name}'
+
+    def get_product_sizes(self):
+        return ProductSize.objects.filter(product=self)
+
+# Model for sizes of physical product
+class ProductSize(models.Model):
+    size = models.CharField(max_length=150)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.size} ${self.price} - {self.product.__str__()}'
+
+# Model for designs that can be used with product
+class Design(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(max_length=250, null=True, blank=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    image = models.ImageField(upload_to='inventory/designs')
+
+    def __str__(self):
+        return f'{self.name} - ${self.price}'
+
+    def image_preview(self):
+        return format_html(f'<img src="{self.image.url}" style="max-width:200px; max-height: 200px"/>')
+  
+# Model for purchases of product and/or designs 
+class Purchase(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    notes = models.TextField(max_length=250, null=True, blank=True)
+    products = models.ManyToManyField(Product, through='PurchaseProduct')
+    designs = models.ManyToManyField(Design, through='PurchaseDesign')
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f'143DPUR{self.pk}'
+
+    def calculate_subtotal(self):
+        purchase_products = PurchaseProduct.objects.filter(purchase=self)
+        purchase_designs = PurchaseDesign.objects.filter(purchase=self)
+        purchase_items = list(chain(purchase_products, purchase_designs))
+        subtotal = sum(purchase_item.quantity * (purchase_item.product_size.cost if isinstance(purchase_item, PurchaseProduct) else purchase_item.design.cost)for purchase_item in purchase_items)
+        self.subtotal = subtotal
+        self.save()
+    
+# Model for purchase products
+class PurchaseProduct(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.ForeignKey(ProductSize, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.purchase.calculate_subtotal()
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+        self.purchase.calculate_subtotal()
+
+# Model for purchase designs
+class PurchaseDesign(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    design = models.ForeignKey(Design, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.purchase.calculate_subtotal()
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+        self.purchase.calculate_subtotal()
--- main/inventory/tests.py
+++ main/inventory/tests.py
@@ -1,3 +1,74 @@
+from .models import Product, ProductSize, Design, Purchase, PurchaseProduct, PurchaseDesign
+from users.models import Supplier
 from django.test import TestCase
 
-# Create your tests here.
+# Tests for inventory models
+class TestInventoryModels(TestCase):
+
+    def setUp(self):
+        self.product = Product.objects.create(name='name', material='material', color='color')
+        self.size = ProductSize.objects.create(size='size', cost=5.75, price=6.25, product=self.product)
+        self.design = Design.objects.create(name='name', cost=4.25, price=4.75, image='path/test.png')
+        self.supplier = Supplier.objects.create(name='name', location='location')
+        self.purchase = Purchase.objects.create(supplier=self.supplier)
+        self.purchase_product = PurchaseProduct.objects.create(purchase=self.purchase, product=self.product, size=self.size, quantity=1)
+        self.purchase_design = PurchaseDesign.objects.create(purchase=self.purchase, design=self.design, quantity=1)
+
+    ## Tests for product model
+    ### Test for model string method
+    def test_product_str(self):
+        self.assertEqual(str(self.product), f'{self.product.color} {self.product.material} {self.product.name}')
+
+    ### Test for get product sizes method
+    def test_product_sizes(self):
+        sizes = self.product.get_product_sizes()
+        self.assertIn(sizes, self.size)
+
+    ## Tests for product size model
+    ### Test for model string method
+    def test_product_size_str(self):
+        self.assertEqual(str(self.size), f'{self.size.size} ${self.size.price} - {self.size.product.__str__()}')
+
+    ## Tests from design model
+    ### Test for model string method
+    def test_design_str(self):
+        self.assertEqual(str(self.design), f'{self.design.name} - ${self.design.price}')
+
+    ### Test for image preview method
+    def test_image_preview(self):
+        preview = self.design.image_preview()
+        self.assertEqual(preview, f'<img src="{self.design.image.url}" style="max-width:200px; max-height: 200px"/>')
+
+    ## Tests for purchase model
+    ### Test for model string method
+    def test_purchase_str(self):
+        self.assertEqual(str(self.purchase), f'143DPUR{self.purchase.pk}')
+
+    ### Test for calculate subtotal method
+    def test_calculate_subtotal(self):
+        subtotal = self.purchase.calculate_subtotal()
+        self.assertEqual(subtotal, self.size.cost + self.design.cost)
+
+    ## Tests for purchase product model
+    ### Test for save method
+    def test_purchase_product_save(self):
+        PurchaseProduct.objects.create(purchase=self.purchase, product=self.product, size=self.size, quantity=2)
+        self.assertEqual(self.purchase.subtotal, (3 * self.size.cost) + self.design.cost)
+
+    ### Test for delete method
+    def test_purchase_product_delete(self):
+        purchase_product = PurchaseProduct.objects.create(purchase=self.purchase, product=self.product, size=self.size, quantity=2)
+        purchase_product.delete()
+        self.assertEqual(self.purchase.subtotal, self.size.cost + self.design.cost)
+
+    ## Tests for purchase design
+    ### Test for save method
+    def test_purchase_design_save(self):
+        PurchaseDesign.objects.create(purchase=self.purchase, design=self.design, quantity=3)
+        self.assertEqual(self.purchase.subtotal, (4 * self.design.cost) + self.size.cost)
+
+    ### Test for delete method
+    def test_purchase_design_delete(self):
+        purchase_design = PurchaseDesign.objects.create(purchase=self.purchase, design=self.design, quantity=3)
+        purchase_design.delete()
+        self.assertEqual(self.purchase.subtotal, self.size.cost + self.design.cost)
