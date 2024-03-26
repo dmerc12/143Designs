466392e4a3b7d4f89dd2e32f13dad21b61e22a73
unknown <dylanmercer12@icloud.com>
2024-03-26 18:12:05 -0500

    adds admin ability to reset admin passwords

--- main/users/urls.py
+++ main/users/urls.py
@@ -1,4 +1,4 @@
-from .views import login_user, logout_user, register, register_admin, admin_home, update_user, delete_user, activate_admin, deactivate_admin, change_password
+from .views import login_user, logout_user, register, register_admin, admin_home, update_user, delete_user, activate_admin, deactivate_admin, change_password, admin_reset_password
 from django.urls import path
 
 # URL's for Users app
@@ -12,5 +12,6 @@ urlpatterns = [
     path('delete/', delete_user, name='delete-user'),
     path('admin/activate/<int:admin_id>', activate_admin, name='activate-admin'),
     path('admin/deactivate/<int:admin_id>', deactivate_admin, name='deactivate-admin'),
+    path('admin/reset/password/<int:user_id>', admin_reset_password, name='admin-reset-password'),
     path('change/password/', change_password, name='change-password')
 ]
