1e68a905695368bc9a85dda8f82013c6702aa106
unknown <dylanmercer12@icloud.com>
2024-03-26 18:23:21 -0500

    adds test coverage for admin reset password functionality

--- main/users/tests.py
+++ main/users/tests.py
@@ -1,4 +1,4 @@
-from .forms import LoginForm, RegisterForm, UpdateUserForm, ChangePasswordForm
+from .forms import LoginForm, RegisterForm, UpdateUserForm, ChangePasswordForm, AdminChangePasswordForm
 from .models import Address, CustomUser, Supplier
 from django.contrib.messages import get_messages
 from django.contrib.auth.models import User
@@ -266,6 +266,40 @@ class TestUsersForms(TestCase):
 
     ### Test change password form validation success
     def test_change_password_form_validation_success(self):
+        data = {'new_password1': 'updatedpass123', 'new_password2': 'updatedpass123'}
+        form = ChangePasswordForm(user=self.base, data=data)
+        self.assertTrue(form.is_valid())
+
+    ## Tests for admin change password form
+    ### Test admin change password form initialization
+    def test_admin_change_password_form_initialization(self):
+        form = ChangePasswordForm(user=self.base)
+        self.assertIn('new_password1', form.fields.keys())
+        self.assertIn('new_password2', form.fields.keys())
+
+    ### Test admin change password form validation with empty fields
+    def test_admin_change_password_form_validation_empty_fields(self):
+        data = {'new_password1': '', 'new_password2': ''}
+        form = ChangePasswordForm(user=self.base, data=data)
+        self.assertIn('new_password1', form.errors)
+        self.assertIn('new_password2', form.errors)
+
+    ### Test admin change password form validation with an invalid password
+    def test_admin_change_password_form_validation_invalid_password(self):
+        data = {'new_password1': self.base.first_name, 'new_password2': self.base.first_name}
+        form = ChangePasswordForm(user=self.base, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password2', form.errors)
+
+    ### Test admin change password form validation with mismatching passwords
+    def test_admin_change_password_form_validation_mismatching_passwords(self):
+        data = {'new_password1': 'mismatching', 'new_password2': 'passwords'}
+        form = ChangePasswordForm(user=self.base, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password2', form.errors)
+
+    ### Test admin change password form validation success
+    def test_admin_change_password_form_validation_success(self):
         data = {'new_password1': 'updatedpass123', 'new_password2': 'updatedpass123'}
         form = ChangePasswordForm(user=self.base, data=data)
         self.assertTrue(form.is_valid())
@@ -544,3 +578,33 @@ class TestUsersViews(TestCase):
         self.assertRedirects(response, reverse('admin-home'))
         messages = [message.message for message in get_messages(response.wsgi_request)]
         self.assertIn(f'The profile for {self.base1.first_name} {self.base1.last_name} has been deactivated!', messages)
+
+    ## Tests for admin reset password view
+    ### Test admin reset password view redirect
+    def test_admin_reset_password_view_redirect(self):
+        response = self.client.get(reverse('admin-reset-password', args=[self.user1.pk]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+        messages = [message.message for message in get_messages(response.wsgi_request)]
+        self.assertIn('You must be a site admin access this page!', messages)
+
+    ### Test admin reset password view rendering success
+    def test_admin_reset_password_view_rendering_success(self):
+        self.client.force_login(self.base3)
+        response = self.client.get(reverse('admin-reset-password', args=[self.user1.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/admin/reset_password.html')
+        self.assertIsInstance(response.context['form'], AdminChangePasswordForm)
+
+    ### Test admin reset password view success
+    def test_admin_reset_password_view_success(self):
+        data = {
+            'new_password1': 'resetpassword',
+            'new_password2': 'resetpassword'
+        }
+        self.client.force_login(self.base3)
+        response = self.client.post(reverse('admin-reset-password', args=[self.user1.pk]), data=data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('admin-home'))
+        messages = [message.message for message in get_messages(response.wsgi_request)]
+        self.assertIn(f'The password for {self.base1.first_name} {self.base1.last_name} has been reset!', messages)
